860c9e74d765b68e5ca6015b21410b538e4220cc
helges1 <sebhelg@gmail.com>
2024-02-27 14:34:31 +0100

    Formaterte litt på shipController

--- src/main/java/controller/ShipController.java
+++ src/main/java/controller/ShipController.java
@@ -73,49 +73,42 @@ public class ShipController implements InputProcessor {
         }
         return true;
     }
-
-
-    // Sebastian: Endret alle til return false for å unngå å få feilmeldinger.
-
-    @Override
-    public boolean keyTyped(char character) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
+    
     @Override
     public boolean mouseMoved(int screenX, int screenY) {
         ship.rotateShip();
         return true;
     }
 
+    // Sebastian: Endret alle som ikke brukes til return false for å unngå å få feilmeldinger.
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
     @Override
     public boolean scrolled(float amountX, float amountY) {
-        // TODO Auto-generated method stub
         return false;
     }
 
