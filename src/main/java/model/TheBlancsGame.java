eb5b42cfa3b9137c0eb33e93bbdc39f2b03fd2a8
Michal <mizbo4705@uib.no>
2024-03-12 12:05:19 +0100

    la til kode i Homescreen og gjorde litt endringer i theBlancsGame

--- src/main/java/model/TheBlancsGame.java
+++ src/main/java/model/TheBlancsGame.java
@@ -1,15 +1,19 @@
 package model;
 
 import com.badlogic.gdx.Game;
-
 import view.GameScreen;
+import view.Homescreen;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class TheBlancsGame extends Game {
 	
 	private GameScreen gameScreen;
+	public SpriteBatch batch;
 
 	@Override
 	public void create() {
+		batch = new SpriteBatch();
 		gameScreen = new GameScreen();
 		super.setScreen(gameScreen);
 	}
--- src/main/java/view/Homescreen.java
+++ src/main/java/view/Homescreen.java
@@ -6,57 +6,101 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.*;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import model.TheBlancsGame;
+
+
 
 
 public class Homescreen implements Screen {
     
-    private SpriteBatch batch;
-    private Texture img;
-
-/*     private static final int WIDTH = 300;
-    private static final int HEIGHT = 80;
+    // endre på størrelsen til knappene og spillet 
+    private static final int PLAY_BUTTON_Y = 300;
     private static final int EXIT_BUTTON_Y = 100;
-    private static final int PLAY_BUTTON_Y = 200; */
+    private static final int PLAY_WIDTH = 300;
+    private static final int PLAY_HEIGHT = 100;
+    private static final int EXIT_WIDTH = 300;
+    private static final int EXIT_HEIGHT = 100; 
+
+    // kan noen se over denne
+    TheBlancsGame game;
     
+    
+    Texture playButtonActive;
+    Texture playButtonInactive;
+    Texture exitButtonActive;
+    Texture exitButtonInactive;
+    Texture background;
+
+    Music menuMusic;
+
 
     public Homescreen() {
-     /*    batch = new SpriteBatch();
-        img = new Texture("homescreen.png"); */
+        this.game = game;
+
+        Gdx.graphics.setWindowedMode(800, 600);
+        Gdx.graphics.setResizable(false);
+
+        //playButtonActive = new Texture("pictures/play_button_active.png");
+
+
+
+        // Load the images for the play and exit buttons and the background
+     
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
+       Gdx.gl.glClearColor(0, 0, 0, 1);
+         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+
+            game.batch.begin();
+            
+            game.batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+            int x = Gdx.graphics.getWidth() / 2 - PLAY_WIDTH / 2;
+
+            if(Gdx.input.getX() < x + PLAY_WIDTH && Gdx.input.getX() > x && Gdx.graphics.getHeight() - Gdx.input.getY() < PLAY_BUTTON_Y + PLAY_HEIGHT && Gdx.graphics.getHeight() - Gdx.input.getY() > PLAY_BUTTON_Y) {
+                game.batch.draw(playButtonActive, x, PLAY_BUTTON_Y, PLAY_WIDTH, PLAY_HEIGHT);
+                if(Gdx.input.isTouched()) {
+                    this.dispose();
+                    game.setScreen(new GameScreen());
+                }
+            } else {
+                game.batch.draw(playButtonInactive, x, PLAY_BUTTON_Y, PLAY_WIDTH, PLAY_HEIGHT);
+            }
+
+            game.batch.end();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        // TODO Auto-generated method stub
     }
 
     @Override
     public void show() {
+        // TODO Auto-generated method stub
     }
 
     @Override
     public void hide() {
+        // TODO Auto-generated method stub
     }
 
     @Override
     public void pause() {
+        // TODO Auto-generated method stub
     }
 
     @Override
     public void resume() {
+        // TODO Auto-generated method stub
     }
 
     @Override
     public void dispose() {
+        menuMusic.dispose();
         
     }
 }
