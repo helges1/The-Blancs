e7f6d1203d95e4786b8805bc84304bd9a1b51535
Sebastian Helgesen <sebhelg@gmail.com>
2024-05-01 18:43:44 +0200

    Used playerShip as the logo for the game

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.Game;
 
 import model.TheBlancsGame;
 
+
+
+
 /**
  * The main entry point for the application. This class is responsible for setting up
  * the application configuration and launching the game.
@@ -14,17 +17,21 @@ import model.TheBlancsGame;
 public class Main extends Game {
 
 	/*
-	 * This is the main method of the application. 
+	 * This is the main method of the application.
 	 * It creates a new Lwjgl3ApplicationConfiguration object and sets the configuration for the application.
-	 *  It sets the title of the application, the window size, and the foreground FPS. 
+	 *  It sets the title of the application, the window size, and the foreground FPS.
 	 * It then creates a new Lwjgl3Application object with the TheBlancsGame class and the configuration object as parameters.
 	 * @param args the command line arguments
 	 */
+
+
+
 	public static void main(String[] args) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("TheBlancs");
-		config.setWindowedMode(1280, 920);
+		config.setWindowedMode(1200, 900);
+		config.setWindowIcon("pictures/playerShip.png");
 		new Lwjgl3Application(new TheBlancsGame(), config);
 	}
 
