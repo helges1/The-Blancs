8fbab6d64335870204edec42601ea7c8df73bd4f
Sebastian Helgesen <sebhelg@gmail.com>
2024-05-03 17:50:14 +0200

    Tester for Main klassen
    Kanskje smartere å bruke mock?

---
+++ src/test/java/main/MainTest.java
@@ -0,0 +1,17 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void testMain() {
+        assertDoesNotThrow(() -> Main.main(new String[]{}));
+    }
+    @Test
+    void create() {
+        Main main = new Main();
+        assertDoesNotThrow(main::create);
+    }
+}
