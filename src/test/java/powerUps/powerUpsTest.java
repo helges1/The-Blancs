9fda0f52bc3c645062620b4f8d44fdf6c4039de0
Michal <mizbo4705@uib.no>
2024-05-03 19:07:11 +0200

    added more to powerUpsTest

--- src/test/java/powerUps/powerUpsTest.java
+++ src/test/java/powerUps/powerUpsTest.java
@@ -2,11 +2,19 @@ package powerUps;
 
 import org.junit.jupiter.api.Test;
 
+
+
 import model.powerUps.PowerUps;
+import model.powerUps.PowerUps.PowerUpType;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+
+
 public class powerUpsTest {
+    
+
 
     @Test
     public void testGetTextureName() {
@@ -23,4 +31,28 @@ public class powerUpsTest {
         assertEquals("Blast Activated", PowerUps.PowerUpType.BLAST.getPowerUpName());
         assertEquals("Gun Upgraded", PowerUps.PowerUpType.GUN.getPowerUpName());
     }
+
+    @Test
+    public void testGetRandomPowerUpType() {
+        PowerUpType randomPowerUpType = PowerUpType.getRandomPowerUpType();
+        assertNotNull(randomPowerUpType, "The random power up type should not be null");
+    }
+
+    @Test
+    public void testGetHeight() {
+        assertEquals(100, PowerUps.PowerUpType.SHIELD.getHeight());
+        assertEquals(100, PowerUps.PowerUpType.LIFE.getHeight());
+        assertEquals(100, PowerUps.PowerUpType.BLAST.getHeight());
+        assertEquals(100, PowerUps.PowerUpType.GUN.getHeight());
+    }
+
+    @Test
+    public void testGetWidth() {
+        assertEquals(100, PowerUps.PowerUpType.SHIELD.getWidth());
+        assertEquals(100, PowerUps.PowerUpType.LIFE.getWidth());
+        assertEquals(100, PowerUps.PowerUpType.BLAST.getWidth());
+        assertEquals(100, PowerUps.PowerUpType.GUN.getWidth());
+    }
+
+
 }
