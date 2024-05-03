eee933350863d275176bd5c3b85dcb094f84a09e
Lasse Holt <lasholt@online.no>
2024-05-03 17:17:01 +0200

    Deleted unused imports

--- src/main/java/model/Asteroid.java
+++ src/main/java/model/Asteroid.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
 /**
--- src/test/java/model/AsteroidTest.java
+++ src/test/java/model/AsteroidTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
